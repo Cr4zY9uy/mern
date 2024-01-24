3a3a76ba6a32ae5ba2631fcfe3586b215b3c2bdd
Cr4zY9uy <heronakamura123@gmail.com>
2024-01-24 14:17:35 +0700

    change header in admin

--- admim-panel/src/services/api.js
+++ admim-panel/src/services/api.js
@@ -3,7 +3,7 @@ const api = axios.create({
     baseURL: `http://localhost:5000/api/`
 });
 if (JSON.parse(localStorage?.getItem("user"))?.jwt?.access_token) {
-    api.defaults.headers.common["Authorization"] = `${JSON.parse(localStorage.getItem("user")).jwt.access_token}`;
+    api.defaults.headers.common["x-authorization"] = `${JSON.parse(localStorage.getItem("user")).jwt.access_token}`;
 }
 
 export default api;
